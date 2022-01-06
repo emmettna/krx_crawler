a622cc6d9486b4bec1ecadc32542c71733d9c5fc
emmettna <lovesalang72@gmail.com>
2022-01-06 23:34:05 +0900

    Fix

--- client/PostgreSQLClient.py
+++ client/PostgreSQLClient.py
@@ -44,11 +44,11 @@ ON CONFLICT (id) DO NOTHING""")
         today_str = today.strftime("%Y-%m-%d")
         ten_years_ago_str = (today - relativedelta(years=10)).strftime("%Y-%m-%d")
         cur.execute(f"""
-        INSERT INTO "korean_stock_base_value_average" (id, date, name, isu, per, pbr, bps, dividend_yield, eps) 
+        INSERT INTO "korean_stock_base_value_average" (id, date, name, isu, per, pbr, bps, dividend_yield, eps, dps) 
             WITH avg_table AS(
-                SELECT '{today_str}'::DATE, isu, AVG(per) AS per, AVG(pbr) AS pbr, AVG(bps) as bps, AVG(dividend_yield) AS dividend_yield, AVG(eps) AS eps FROM korean_stock_base_value 
+                SELECT '{today_str}'::DATE, isu, AVG(per) AS per, AVG(pbr) AS pbr, AVG(bps) as bps, AVG(dividend_yield) AS dividend_yield, AVG(eps) AS eps , AVG(dps) as dps FROM korean_stock_base_value 
                 WHERE date > '{ten_years_ago_str}' AND date <= '{today_str}' GROUP BY isu)
-            SELECT date || '-' || isu AS id, date, (select name from korean_stock_base_value where isu = avg_table.isu limit 1), isu, per, pbr, bps,dividend_yield, eps FROM avg_table
+            SELECT date || '-' || isu AS id, date, (select name from korean_stock_base_value where isu = avg_table.isu limit 1), isu, per, pbr, bps,dividend_yield, eps, dps FROM avg_table
         ON CONFLICT (id) DO NOTHING""")
         cur.close()
         conn.commit()
