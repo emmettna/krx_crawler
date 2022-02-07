3124f47e3227944fb23c4a367f403282b5e737d4
emmettna <lovesalang72@gmail.com>
2022-02-06 20:50:13 -0500

    Fix

--- main.py
+++ main.py
@@ -82,14 +82,15 @@ def main():
         if (pg_client != None): await asyncio.create_task(pg_client.save_stock(stock_rows, pg_conn))
         if (es_client != None): await asyncio.create_task(ElasticSearch.save(stock_rows, 'stock', es_client))
         if (local_storage): await asyncio.create_task(save(stock_rows, local_download_parent_dir + '/stock'))
-        # if(firestore_client != None): await asyncio.create_task(Firestore.upload_to_firestore(stock_rows, today, firestore_client))
+        if(firestore_client != None): await asyncio.create_task(Firestore.upload_to_firestore(stock_rows, today, firestore_client))
         
         stock_base_values_rows = download_stock_base_values(today)
-        if (pg_client != None): await asyncio.create_task(pg_client.save_stock_base_values(stock_base_values_rows, pg_conn))
-        if (pg_client != None): await asyncio.create_task(pg_client.save_stock_base_value_avg(pg_conn, today))
-        if (pg_client != None): await asyncio.create_task(pg_client.upsert_under_valued_assets_to_cache(pg_conn, today))
-        if (es_client != None): await asyncio.create_task(ElasticSearch.save(stock_base_values_rows, 'stock_values', es_client))
-        if (local_storage): await asyncio.create_task(save(stock_base_values_rows, local_download_parent_dir + '/stock_values'))
+        if(sum((row.end_price for row in stock_base_values_rows)) > 0):
+            if (pg_client != None): await asyncio.create_task(pg_client.save_stock_base_values(stock_base_values_rows, pg_conn))
+            if (pg_client != None): await asyncio.create_task(pg_client.save_stock_base_value_avg(pg_conn, today))
+            if (pg_client != None): await asyncio.create_task(pg_client.upsert_under_valued_assets_to_cache(pg_conn, today))
+            if (es_client != None): await asyncio.create_task(ElasticSearch.save(stock_base_values_rows, 'stock_values', es_client))
+            if (local_storage): await asyncio.create_task(save(stock_base_values_rows, local_download_parent_dir + '/stock_values'))
 
         etf_rows = download_etf(today)
         if(pg_client != None): await asyncio.create_task(pg_client.save_eft(etf_rows, pg_conn))
